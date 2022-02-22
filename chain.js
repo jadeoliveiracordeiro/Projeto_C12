b54590326faca8a0499708f3096403e7855e5c3c
Jade Oliveira <80401565+jadeoliveiracordeiro@users.noreply.github.com>
2022-02-22 16:24:30 -0300

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,14 @@
+class Chain{
+    constructor(bodya,bodyb){
+        var options = {
+            bodyA: 
+            bodyB: 
+            stiffness: 
+            length:
+        }
+        
+    }
+    display(){
+        
+    }
+}
